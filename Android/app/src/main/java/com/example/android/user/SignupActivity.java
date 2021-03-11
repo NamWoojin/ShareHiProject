71c41b690a00510b95efe62b83c00e19288bc926
NamWoojin <nam990304@gmail.com>
2021-03-11 21:08:00 +0900

    Add S04FIELD01-81 : 회원가입 화면 구현

--- Android/app/src/main/java/com/example/android/user/SignupActivity.java
+++ Android/app/src/main/java/com/example/android/user/SignupActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.user;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.android.R;
+import com.google.android.gms.common.SignInButton;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -12,5 +14,9 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_signup);
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.activity_signup_google_button);
+        TextView textView = (TextView) signInButton.getChildAt(0);
+        textView.setText("Google 이메일로 가입하기");
     }
 }
