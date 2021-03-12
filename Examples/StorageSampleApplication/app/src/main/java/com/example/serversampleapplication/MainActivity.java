9320f01320fadca0140901cb7fc81fd703c90eb4
NamWoojin <nam990304@gmail.com>
2021-03-12 18:01:30 +0900

    Update S04FIELD01-87 : Android Storage 전체 접근 테스트

--- Examples/StorageSampleApplication/app/src/main/java/com/example/serversampleapplication/MainActivity.java
+++ Examples/StorageSampleApplication/app/src/main/java/com/example/serversampleapplication/MainActivity.java
@@ -25,13 +25,11 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity { // public class MainActivity extends Activity {
 
 
-
     private String mFileName;
 
     private ListView lvFileControl;
 
     private Context mContext = this;
-
 
 
     private List<String> lItem = null;
@@ -43,7 +41,6 @@ public class MainActivity extends AppCompatActivity { // public class MainActivi
     private TextView mPath;
 
 
-
     @Override
 
     public void onCreate(Bundle savedInstanceState) {
@@ -53,106 +50,60 @@ public class MainActivity extends AppCompatActivity { // public class MainActivi
         setContentView(R.layout.activity_main);
 
         mPath = (TextView) findViewById(R.id.tvPath);
-
-        lvFileControl = (ListView)findViewById(R.id.lvFileControl);
+        lvFileControl = (ListView) findViewById(R.id.lvFileControl);
 
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             //권한 허용되지 않았으면
-           ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         }
+
         getDir(mRoot);
         lvFileControl.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-
                 File file = new File(lPath.get(position));
-
-
-
                 if (file.isDirectory()) {
-
                     if (file.canRead())
-
                         getDir(lPath.get(position));
-
                     else {
-
                         Toast.makeText(mContext, "No files in this folder.", Toast.LENGTH_SHORT).show();
-
                     }
-
                 } else {
-
                     mFileName = file.getName();
-
                     Log.i("Test", "ext:" + mFileName.substring(mFileName.lastIndexOf('.') + 1, mFileName.length()));
-
                 }
-
             }
-
         });
-
     }
 
 
-
     private void getDir(String dirPath) {
-
         mPath.setText("Location: " + dirPath);
-
-
-
         lItem = new ArrayList<String>();
-
         lPath = new ArrayList<String>();
 
-
-
         File f = new File(dirPath);
-
         File[] files = f.listFiles();
 
-
-
         if (!dirPath.equals(mRoot)) {
-
             //item.add(root); //to root.
-
             //path.add(root);
-
             lItem.add("../"); //to parent folder
-
             lPath.add(f.getParent());
-
         }
 
-//        if(files != null) {
-            for (int i = 0; i < files.length; i++) {
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            lPath.add(file.getAbsolutePath());
 
-                File file = files[i];
-
-                lPath.add(file.getAbsolutePath());
-
-
-                if (file.isDirectory())
-
-                    lItem.add(file.getName() + "/");
-
-                else
-
-                    lItem.add(file.getName());
-
-            }
-//        }
-
-
-
+            if (file.isDirectory())
+                lItem.add(file.getName() + "/");
+            else
+                lItem.add(file.getName());
+        }
         ArrayAdapter<String> fileList = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lItem);
-
         lvFileControl.setAdapter(fileList);
-
     }
 
 }
