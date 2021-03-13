aa02b6fbab198c63f3ad199fcdbeb40e6bcae974
구민진 <whxorb44@gmail.com>
2021-03-13 16:08:48 +0900

    Update S04FIELD01-55 : app.js 서버실행변경

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,8 +2,11 @@
 
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
 const home = require('./controller/home');
 app.use('/', home);
 
-module.exports = app;
+app.listen(PORT, function () {
+  console.log('서버가동');
+});
