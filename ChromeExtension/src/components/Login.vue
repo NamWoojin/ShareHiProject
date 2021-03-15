2db7101dc86d056cf9ad4c266b6e91d80ac00b59
eric <eric99kr@naver.com>
2021-03-16 00:33:30 +0900

    Add S04FIELD01-83 : 크롬익스텐션 개발 위한 환경 설정과 기본 구조 빌드

---
+++ ChromeExtension/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login-container">
+    <img id="logo-image" src="" alt="">
+    <input class="login-email-input" placeholder="이메일">
+    <input class="login-password-input" placeholder="비밀번호">
+    <button class="login-btn" @click="onClickLogin">로그인</button>
+    <footer>
+      <button>회원가입</button>
+      <button>비밀번호 찾기</button>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  methods : {
+    onClickLogin() {
+      alert('Clicked Login')
+      this.$emit("onClickLogin",true)
+    },
+  },
+  created () {
+  }
+};
+</script>
---
+++ ChromeExtension/src/components/Main.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <nav class="nav">
+      <div class="nav-item" >
+        <button @click="onClickNavItem" class="nav-item-btn">알림</button>
+        <div v-if="true" class="nav-item-content">
+            <a href="#">Alert item1</a>
+            <a href="#">Alert item2</a>
+            <a href="#">Alert item3</a>
+            <a href="#">Alert item4</a>
+        </div>
+        <div v-else>
+          알림이 없습니다
+        </div>
+      </div>
+      <div class="nav-item" >
+        <button @click="onClickNavItem" class="nav-item-btn">디바이스</button>
+        <div v-if="true" class="nav-item-content">
+            <a href="#">Device1</a>
+            <a href="#">Device2</a>
+            <a href="#">Device3</a>
+            <a href="#">Device4</a>
+        </div>
+        <div v-else>
+          알림이 없습니다
+        </div>
+      </div>
+      <div class="nav-item" >
+        <button @click="onClickNavItem" class="nav-item-btn">+</button>
+        <div class="nav-item-content nav-item-last">
+            <a href="#">계정설정</a>
+            <a href="#" @click="onClickLogout">로그아웃</a>
+        </div>
+      </div>
+    </nav>
+    <div class="directory">
+      장치를 연결해주세요
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  methods : {
+    onClickNavItem(e) {
+      const clickedNavItemContent = e.target.nextSibling
+      const navItemContents = document.querySelectorAll('.nav-item-content')
+      for (let idx = 0; idx<navItemContents.length; idx++ ) {
+        if (clickedNavItemContent !== navItemContents[idx]) {
+          navItemContents[idx].classList.remove('show')
+        }
+      }
+      clickedNavItemContent.classList.toggle('show')
+    },
+    onClickLogout() {
+      alert("logout")
+      this.$emit("onClickLogout",false)
+    }
+  },
+  mounted() {
+    window.addEventListener('click', (e)=>{
+      const navItemContents = document.querySelectorAll('.nav-item-content')
+      if (e.target.nextSibling && e.target.nextSibling.classList.contains("nav-item-content")) {
+        return
+      }
+      else {
+        for (let idx = 0; idx<navItemContents.length; idx++ ) {
+          navItemContents[idx].classList.remove('show')
+        }
+      }
+    })
+  }
+};
+</script>
+
