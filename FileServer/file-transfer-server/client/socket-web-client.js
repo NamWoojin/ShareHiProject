942374b39b965a936985ef47655cf43b1d6aacd1
jaehyukshim <jaehyukawot@gmail.com>
2021-03-19 14:21:51 +0900

    Add S04FIELD01-116 Net module, Socket.io module
    Former-commit-id: ab1ce50e2a1b73bfdd3f44ae98ed3d41e3997d58

--- FileServer/file-transfer-server/client/socket-web-client.js
+++ FileServer/file-transfer-server/client/socket-web-client.js
@@ -7,102 +7,101 @@ const io = require('socket.io-client');
 const ss = require('socket.io-stream');
 
 let config = {
-    host: 'http://127.0.0.1:8765',
-    uploadedSize: 0,
-    uploadTotalSize: 0,
-    savePath: './',
-    saveName: 'sample',
-    saveExt: '.mp4',
-    saveFile: '',
-    filename: '',
-    downLoadSize: 0,
-    downLoadTotalSize: 0,
-}
+  host: 'http://127.0.0.1:8765',
+  uploadedSize: 0,
+  uploadTotalSize: 0,
+  savePath: './',
+  saveName: 'sample',
+  saveExt: '.mp4',
+  saveFile: '',
+  filename: '',
+  downLoadSize: 0,
+  downLoadTotalSize: 0,
+};
 let socket = '';
 
 let getConnection = function () {
-    socket = io.connect(config.host, {
-        reconnectionDelayMax: 1000,
-        auth: {
-            token: "123"
-        },
-        query: {
-            "my-key": "my-value"
-        }
-    });
-    socket.on('connect', () => {
-        if (socket.connected)
-            console.log('서버로 성공적으로 연결되었습니다 : ' + config.host);
-        else
-            console.log('서버의 연결이 끊겼습니다 : ' + config.host);
-    });
-    socket.on('message', recvMessageData);
-    socket.on('blob', recvBlob);
-}
+  socket = io.connect(config.host, {
+    reconnectionDelayMax: 1000,
+    auth: {
+      token: '123',
+    },
+    query: {
+      'my-key': 'my-value',
+    },
+  });
+  socket.on('connect', () => {
+    if (socket.connected) console.log('서버로 성공적으로 연결되었습니다 : ' + config.host);
+    else console.log('서버의 연결이 끊겼습니다 : ' + config.host);
+  });
+  socket.on('message', recvMessageData);
+  socket.on('blob', recvBlob);
+};
 //getConnection();
 
 let sendData = (id, data) => {
-    socket.emit(id, data);   
-    console.log('서버로 데이터를 전송하였습니다.');
-}
+  socket.emit(id, data);
+  console.log('서버로 데이터를 전송하였습니다.');
+};
 
 //sendData('message', 'hello man~');
 
 let recvMessageData = (data) => {
-    console.log('서버에서 데이터를 받아 리턴합니다');
-    console.log(data);
-    return data;
-}
+  console.log('서버에서 데이터를 받아 리턴합니다');
+  console.log(data);
+  return data;
+};
 
 let setFileName = (filename) => {
-    config.filename = filename;
-    console.log('보낼 파일의 이름을 변경합니다');
-}
+  config.filename = filename;
+  console.log('보낼 파일의 이름을 변경합니다');
+};
 //setFileName('./img/sample.mp4');
 
 let sendBlob = (fileName) => {
-    setFileName(fileName);
-    let file = fs.createReadStream(config.filename, {flags: 'r'});
-    var stats = fs.statSync(config.filename);
-    config.uploadTotalSize = stats.size;
-    config.uploadedSize = 0;
-    console.log('서버로 대용량 파일을 전송합니다');
-    file.on('data', (data) => {
-        config.uploadedSize += data.length;
-        sendData('blob', data);
-        getFilePercent();
-      });
-}
+  setFileName(fileName);
+  let file = fs.createReadStream(config.filename, { flags: 'r' });
+  var stats = fs.statSync(config.filename);
+  config.uploadTotalSize = stats.size;
+  config.uploadedSize = 0;
+  console.log('서버로 대용량 파일을 전송합니다');
+  file.on('data', (data) => {
+    config.uploadedSize += data.length;
+    sendData('blob', data);
+    getFilePercent();
+  });
+};
 
 let getFilePercent = () => {
-    let ans = (Math.floor((config.uploadedSize / config.uploadTotalSize)*100));
-    console.log(ans);
-    return ans;
-}
+  let ans = Math.floor((config.uploadedSize / config.uploadTotalSize) * 100);
+  console.log(ans);
+  return ans;
+};
 //sendBlob('./img/sample.mp4');
 
-let setPath = (path,name,ext) => {
-    config.savePath = path;
-    config.saveName = name;
-    config.saveExt = ext;
-    config.saveFile = fs.createWriteStream(config.savePath + config.saveName + config.saveExt, { flags: 'w' });
-    console.log('받을 파일의 경로를 수정했습니다');
+let setPath = (path, name, ext) => {
+  config.savePath = path;
+  config.saveName = name;
+  config.saveExt = ext;
+  config.saveFile = fs.createWriteStream(config.savePath + config.saveName + config.saveExt, {
+    flags: 'w',
+  });
+  console.log('받을 파일의 경로를 수정했습니다');
 };
 
 let recvBlob = (data) => {
-    config.saveFile.write(data);
+  config.saveFile.write(data);
 };
 
 //setPath('./save/', 'sample', '.mp4');
 
 module.exports = {
-    getConnection: getConnection,
-    sendData: sendData,
-    recvMessageData: recvMessageData,
-    setFileName: setFileName,
-    sendBlob: sendBlob,
-    getFilePercent: getFilePercent,
-    setPath: setPath,
-    recvBlob: recvBlob,
-    
-  };
+  getConnection: getConnection,
+  sendData: sendData,
+  recvMessageData: recvMessageData,
+  setFileName: setFileName,
+  sendBlob: sendBlob,
+  getFilePercent: getFilePercent,
+  setPath: setPath,
+  recvBlob: recvBlob,
+};
--- FileServer/file-transfer-server/server/socket-server.js
+++ FileServer/file-transfer-server/server/socket-server.js
@@ -2,8 +2,10 @@
 // 지금은 일단 모듈화
 
 let config = {
-    host: 'localhost',
-}
+  host: 'localhost',
+};
+
+const net = require('net');
 
 const http = require('http');
 const fs = require('fs');
@@ -15,22 +17,58 @@ const app = express();
 
 let server = http.createServer(app);
 server.listen(8765, () => {
-    console.log('Server Running at http://127.0.0.1:8765');
+  console.log('Server Running at http://127.0.0.1:8765');
 });
 
 let io = socketio(server);
 
 io.sockets.on('connection', (socket) => {
-
-    socket.on('message', (data) => {
-        console.log(data);
-        writeData(socket, 'message', 'hi');
-    });
-    socket.on('blob', (data) => {
-        writeData(socket, 'blob', data);
-    });
+  socket.on('message', (data) => {
+    console.log(data);
+    writeData(socket, 'message', data);
+  });
+  socket.on('blob', (data) => {
+    writeData(socket, 'blob', data);
+  });
 });
 
 let writeData = function (socket, id, data) {
-    socket.emit(id, data);
-}
+  socket.emit(id, data);
+};
+
+var andServer = net.createServer(function (client) {
+  client.on('data', function (data) {
+    console.log('------ writing to client: ---------');
+    andWriteData(client, data);
+    console.log(data);
+    console.log('...........................');
+  });
+
+  client.on('end', function () {
+    console.log('is end?');
+    nfile.on('data', (data) => {
+      console.log('-----------');
+      andWriteData(client, data);
+      console.log('...........................');
+    });
+  });
+
+  client.on('error', function (err) {});
+
+  client.on('timeout', function () {});
+
+  client.on('drain', () => {
+    console.log('hi drain~');
+  });
+});
+
+andServer.listen(8888, function () {
+  console.log('Server Running at http://127.0.0.1:8888');
+  andServer.on('close', function () {});
+
+  andServer.on('error', function (err) {});
+});
+
+var andWriteData = function (socket, data) {
+  var success = !socket.write(data);
+};
