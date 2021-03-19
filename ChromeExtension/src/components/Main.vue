80023d0146116a8d71045f14c12c1e9d6c89d17e
eric <eric99kr@naver.com>
2021-03-19 13:43:23 +0900

    Update S04FIELD01-137 : 크롬익스텐션 파일 브러우저 파일 및 폴더 삭제 기능 구현
    Former-commit-id: 2d251bf75eccef404e5b13fc3fe5b4b2728d0d61

--- ChromeExtension/src/components/Main.vue
+++ ChromeExtension/src/components/Main.vue
@@ -42,64 +42,28 @@
           <a href="#" class="dir">폴더1</a>
           <ul>
             <li>
-              <a href="#" class="dir">폴더1-1</a>
-              <ul>
-                <li><a href="#" class="dir">폴더1-1-1</a></li>
-                <li><a href="#" class="dir">폴더1-1-2</a></li>
-                <li><a href="#" class="file">파일1-1-1</a></li>
-              </ul>
-            </li>
-            <li>
               <a href="#" class="dir">폴더1-2</a>                
               <ul>
-                <li><a href="#" class="dir">폴더1-2-1</a></li>
-                <li><a href="#" class="dir">폴더1-2-2</a></li>
-                <li><a href="#" class="file">파일1-2-1</a></li>
-              </ul>
-            </li>
-            <li><a href="#" class="file">파일1-1</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#" class="dir">폴더2</a>
-          <ul>
-            <li><a href="#" class="dir">폴더2-1</a></li>
-            <li><a href="#" class="dir">폴더2-2</a></li>
-            <li><a href="#" class="file">파일2-1</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#" class="dir">폴더3</a>
-          <ul>
-            <li><a href="#" class="dir">폴더3-1</a></li>
-            <li><a href="#" class="dir">폴더3-2</a></li>
-            <li><a href="#" class="file">파일3-1</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#" class="dir">폴더4</a>
-          <ul>
-            <li>
-              <a href="#" class="dir">폴더4-1</a>
-              <ul>
                 <li>
-                  <a href="#" class="dir">폴더4-1-1</a>
+                  <a href="#" class="dir">폴더1-2-1</a>
                   <ul>
-                    <li><a href="#" class="dir">폴더4-1-1-1</a></li>
-                    <li><a href="#" class="dir">폴더4-1-1-2</a></li>
-                    <li><a href="#" class="dir">폴더4-1-1-3</a></li>
+
                   </ul>
-                </li> 
-                <li><a href="#" class="dir">폴더4-1-2</a></li> 
-                <li><a href="#" class="dir">폴더4-1-3</a></li> 
-                <li><a href="#" class="file">파일4-1-1</a></li> 
-                <li><a href="#" class="file">파일4-1-2</a></li> 
-                <li><a href="#" class="file">파일4-1-3</a></li> 
-                <li><a href="#" class="file">파일4-1-4</a></li> 
+                </li>
+                <li>
+                  <a href="#" class="dir">폴더1-2-2</a>
+                  <ul>
+
+                  </ul>
+                </li>
+                <li>
+                  <a href="#" class="file">파일1-2-1</a>
+                </li>
               </ul>
             </li>
-            <li><a href="#" class="dir">폴더4-2</a></li>
-            <li><a href="#" class="file">파일4-1</a></li>
+            <li>
+              <a href="#" class="file">파일1-1</a>
+            </li>
           </ul>
         </li>
         <li>
@@ -135,26 +99,129 @@ export default {
       for (let idx=0; idx<uls.length; idx++) {
         uls[idx].classList.add('closed')
       } 
-    }
-  },
-  mounted() {
-    const directory = document.querySelector('#shadowElement').shadowRoot.querySelector(".directory")
-
-    const uls = directory.querySelectorAll('ul')
-    for (let idx=0; idx<uls.length; idx++) {
-      uls[idx].classList.add('closed')
-    } 
-
-    const dirs = directory.querySelectorAll('.dir')
-    for (let idx=0; idx<dirs.length; idx++) {
-      const ul = dirs[idx].nextElementSibling
-      if (ul) {
+    },
+    addClassToUl() {
+      const directory = document.querySelector('#shadowElement').shadowRoot.querySelector(".directory")
+      const uls = directory.querySelectorAll('ul')
+      for (let idx=0; idx<uls.length; idx++) {
+        uls[idx].classList.add('closed')
+      }
+    },
+    addEventToDir() {
+      const directory = document.querySelector('#shadowElement').shadowRoot.querySelector(".directory")
+      const dirs = directory.querySelectorAll('.dir')
+      const files = directory.querySelectorAll('.file')
+      for (let idx=0; idx<dirs.length; idx++) {
+        const ul = dirs[idx].nextElementSibling
         dirs[idx].addEventListener('click', () => {
           ul.classList.toggle('closed')
         })
+        dirs[idx].addEventListener('contextmenu', (e) => {
+          e.preventDefault();
+          this.createContextMenu(e.clientX,e.clientY,e.target,'dir');
+        })
       }
-    }
+      for (let idx=0; idx<files.length; idx++) {
+        files[idx].addEventListener('contextmenu', (e) => {
+          e.preventDefault();
+          this.createContextMenu(e.clientX,e.clientY,e.target,'file');
+        })
+      }
+    },
+    setUploadMenu(target) {
+      const uploadMenu = document.createElement('div')
+      uploadMenu.setAttribute('class','context-menu-content')
+      uploadMenu.innerText = '파일추가'
+      uploadMenu.addEventListener('click', (e) => {
+        console.log(e,target)
+        this.deleteContextMenu()
+      })
+      return uploadMenu
+    },
+    setDeleteMenu(target) {
+      const deleteMenu = document.createElement('div')
+      deleteMenu.setAttribute('class','context-menu-content')
+      deleteMenu.innerText = '삭제하기'
+      deleteMenu.addEventListener('click', (e) => {
+        if (confirm("정말 삭제하시겠어요?")) {
+          const ul = target.nextElementSibling
+          if (ul) {
+            ul.innerHTML = '';
+            ul.remove()
+          }
+          target.remove()
+          this.deleteContextMenu()
+        }
+        else {
+          this.deleteContextMenu()
+        }
+      })
+      return deleteMenu
+    },
+    setRenameMenu(target) {
+      const reNameMenu = document.createElement('div')
+      reNameMenu.setAttribute('class','context-menu-content')
+      reNameMenu.innerText = '이름변경'
+      reNameMenu.addEventListener('click', (e) => {
+        console.log(e,target)
+        this.deleteContextMenu()
+      })
+      return reNameMenu
+    },
+    setDownloadMenu(target) {
+      const downloadMenu = document.createElement('div')
+      downloadMenu.setAttribute('class','context-menu-content')
+      downloadMenu.innerText = '다운로드'
+      downloadMenu.addEventListener('click', (e) => {
+        console.log(e,target)
+        this.deleteContextMenu()
+      })
+      return downloadMenu
+    },
+    deleteContextMenu() {
+      const contextMenuOverlay = document.querySelector('#shadowElement').shadowRoot.querySelector('.context-menu-overlay')
+      const contextMenu = document.querySelector('#shadowElement').shadowRoot.querySelector('.context-menu')
+      if (contextMenuOverlay) {
+        contextMenuOverlay.remove()
+      }
+      if (contextMenu) {
+        contextMenu.remove()
+      }
+    },
+    createContextMenu(x,y,target,targetType) {
+      const overlay = document.querySelector('#shadowElement').shadowRoot.querySelector('.overlay')
+      const contextMenu = document.createElement('div')
+      const contextMenuOverlay = document.createElement('div')
 
+      contextMenuOverlay.setAttribute('class','context-menu-overlay')
+      contextMenuOverlay.addEventListener('click',()=>{
+        this.deleteContextMenu()
+      })
+      contextMenuOverlay.addEventListener('contextmenu',(e)=>{
+        e.preventDefault();
+        this.deleteContextMenu()
+      })
+
+      contextMenu.setAttribute('class','context-menu')
+      contextMenu.style.left = `${x}px`
+      contextMenu.style.top =  `${y}px`
+      
+
+      contextMenu.appendChild(this.setDeleteMenu(target))
+      contextMenu.appendChild(this.setRenameMenu(target))
+      contextMenu.appendChild(this.setDownloadMenu(target))
+
+      if (targetType === 'dir') {
+        contextMenu.appendChild(this.setUploadMenu(target))
+      }
+      overlay.appendChild(contextMenuOverlay)
+      overlay.appendChild(contextMenu)
+      console.log(x,y,target)
+    }
+  },
+  mounted() {
+    this.addClassToUl()
+    this.addEventToDir()
   }
 };
 </script>
