d11a488ec0e59250087aacd97ba7ce9d0c8b7cfd
구민진 <whxorb44@gmail.com>
2021-03-22 11:31:50 +0900

    Update S04FIELD01-95 : email인증, 회원CRUD
    Former-commit-id: 64235222560ff8031fa911fcf5aa3b9462518136

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -4,8 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 const user = require('./user')
-
+const login = require('./login')
 //router.use('/main', main);
 router.use('/user', user);
+router.use('/login', login);
 
 module.exports = router;
