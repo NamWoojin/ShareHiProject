b17daa471eba79ec61d9a8aa908e5053584e1710
구민진 <whxorb44@gmail.com>
2021-03-24 14:02:57 +0900

    Add S04FIELD01-135 : [Back] 소셜로그인 추가 및 오타수정
    Former-commit-id: 85f0bc0185b5a1b98c2fd9656456a0788c4994a3

--- Backend/src/models/login/login.js
+++ Backend/src/models/login/login.js
@@ -1,9 +1,12 @@
 'use strict'
 
 const basic = 'select * from member where mem_email=? and mem_password=?';
-const socail = 'select * from member where mem_email=? and mem_password=?';
-
+const social = 'select * from member where mem_email=? and mem_password=?';
+const socialCheckEmail = 'select * from member where mem_email=?';
+const insertSocial = 'insert into member set ?';
 module.exports = {
     basic,
-    socail
+    social,
+    socialCheckEmail,
+    insertSocial
 }
