c8e062fefa393bc22386321dd759001508ce1922
구민진 <whxorb44@gmail.com>
2021-03-24 21:36:46 +0900

    S04FIELD01-163 : CORS문제 수정
    Former-commit-id: 30ca8631529a6562d59a90b272ee03008aa34894

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,9 @@ const app = express();
 const home = require('./src/routes');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const cors = require('cors');
+app.use(cors());
 //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 //app.use('/api', home);
 app.use('/api', home);
