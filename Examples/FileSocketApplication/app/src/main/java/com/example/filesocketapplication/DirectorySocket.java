c7e4b53f3ea07f0621e419e8dbbf81ab0b9a4ee7
NamWoojin <nam990304@gmail.com>
2021-03-25 15:52:36 +0900

    Add S04FIELD01-109 : 소켓 통신을 위한 Sample Application 제작
    Former-commit-id: 767d307a51efde88d313582531a964a00500abb0

---
+++ Examples/FileSocketApplication/app/src/main/java/com/example/filesocketapplication/DirectorySocket.java
@@ -0,0 +1,21 @@
+package com.example.filesocketapplication;
+
+import org.json.JSONObject;
+
+public class DirectorySocket {
+
+    private JSONObject jsonObject;
+
+    public DirectorySocket(JSONObject jsonObject){
+        this.jsonObject = jsonObject;
+        directorySendThread.start();
+    }
+
+    private Thread directorySendThread = new Thread(){
+        @Override
+        public void run() {
+            super.run();
+            //
+        }
+    };
+}
---
+++ Examples/FileSocketApplication/app/src/main/java/com/example/filesocketapplication/FileSocket.java
@@ -0,0 +1,17 @@
+package com.example.filesocketapplication;
+
+public class FileSocket {
+
+    public FileSocket(){
+
+        fileSendThread.start();
+    }
+
+    private Thread fileSendThread = new Thread(){
+        @Override
+        public void run() {
+            super.run();
+            //
+        }
+    };
+}
