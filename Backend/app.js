ebd0247ad30d85447b028fdee262a2d64c2fdcb9
jaehyukshim <jaehyukawot@gmail.com>
2021-03-25 23:04:57 +0900

    Add S04FIELD01-166
    Former-commit-id: 17e21dd0a44e0dd52ee00c79e100372efc9a4b91

--- Backend/app.js
+++ Backend/app.js
@@ -4,9 +4,6 @@
 const express = require('express');
 const app = express();
 
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
 // swagger
 // const { swaggerUi, specs } = require('./src/modules/swagger');
 // 라우팅
@@ -20,17 +17,170 @@ app.use(cors());
 //app.use('/api', home);
 app.use('/api', home);
 
-const KEY = require('./src/services/key/key.js');
+//////////////// start jae hyuk code /////////////////
+//////////////////////////////////////////////////////
 
-server.listen(8081);
-let idCount = 0;
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const net = require('net');
+const HashMap = require('hashmap');
+const { v4: uuidv4 } = require('uuid');
+
+const KEY = require('./src/config/key/key.js');
+const { client } = require('./src/config/redis/redis.emailAuth');
+
 let shareDevice;
+//////////////// socket map system /////////////
+let socketMap = new HashMap(); // socket - id
+let idMap = new HashMap(); // id - socket
+///////^^^^^^^^^^^^^^^^^^^^^^^^^^^^/////////////
+
+let andServer = net.createServer((socket) => {
+  /////////// 초기 연결 /////////////////
+  registSocket(socket);
+  socket.write(
+    JSON.stringify({
+      id: KEY.CONNECT,
+      status: 200,
+      message: 'connect',
+    }) + '\n'
+  );
+  printSocket();
+  ////////^^^^^^^^^^^^^^^^^^^^^^^///////
+
+  socket.on('error', (err) => {
+    console.log('socket 연결 끊김 : ' + socketMap.get(socket));
+    deleteSocket(socket);
+    printSocket();
+  });
+  socket.on('end', () => {
+    deleteSocket(socket);
+    console.log('end socket : ' + socket);
+    printSocket();
+  });
+
+  socket.on('data', (data) => {
+    data = JSON.parse(data);
+    console.log(data);
+    let id = parseInt(data.namespace);
+    ///////////////////////////////////////////
+    ///////////// 프로토콜 /////////////////////
+    ///////////////////////////////////////////
+    switch (id) {
+      case KEY.SET_SHARE_ID:
+        // 1020 공유 디바이스의 아이디를 설정한다
+        shareDevice = socketMap.get(socket);
+        console.log('shareDevice : ' + shareDevice);
+        break;
+      case KEY.GET_TREE_OF_FOLDERS:
+        // 2000 폴더 디렉토리 요청에 대한 응답
+        io.to(idMap.get(data.targetId).id).emit(
+          KEY.GET_TREE_OF_FOLDERS,
+          JSON.stringify({
+            status: data.status,
+            message: data.message,
+            data: data.data,
+          })
+        );
+        break;
+      case KEY.UPDATE_NAME_OF_FOLDER:
+        // 2001 폴더 이름 수정에 대한 응답
+        io.to(idMap.get(data.targetId).id).emit(
+          KEY.UPDATE_NAME_OF_FOLDER,
+          JSON.stringify({
+            status: data.status,
+            message: data.message,
+            data: data.data,
+          })
+        );
+        break;
+      case KEY.DELETE_FOLDERS:
+        // 2001 폴더 삭제에 대한 응답
+        io.to(idMap.get(data.targetId).id).emit(
+          KEY.DELETE_FOLDERS,
+          JSON.stringify({
+            status: data.status,
+            message: data.message,
+            data: data.data,
+          })
+        );
+        break;
+      case KEY.RES_ADD_FOLDERS:
+        // 2003 폴더 추가에 대한 응답
+        io.to(idMap.get(data.targetId).id).emit(
+          KEY.RES_ADD_FOLDERS,
+          JSON.stringify({
+            status: data.status,
+            message: data.message,
+            data: data.data,
+          })
+        );
+        break;
+      // 2001 폴더 이름을 변경한 후의 응답
+    }
+    //////////////////////////////////////////
+    //////////////////////////////////////////
+    //////////////////////////////////////////
+
+    shareDevice = socketMap.get(socket);
+  });
+});
+
+////////////// socket map CRUD //////////////////
+let deleteSocket = (socket) => {
+  if (idMap.get(socketMap.get(socket))) {
+    idMap.delete(socketMap.get(socket));
+  }
+  if (socketMap.get(socket)) {
+    if (shareDevice === socketMap.get(socket)) {
+      shareDevice = undefined;
+    }
+    socketMap.delete(socket);
+  } else {
+    return;
+  }
+};
+let registSocket = (socket) => {
+  let uuid = uuidv4();
+  if (!socketMap.get(socket)) {
+    socketMap.set(socket, uuid);
+  } else {
+    return;
+  }
+  idMap.set(uuid, socket);
+};
+let checkSocket = (id) => {
+  if (!idMap.get(id)) return false;
+  return true;
+};
+let printSocket = (socket) => {
+  console.log('---------print--------------');
+  console.log('shareDevice : ' + shareDevice);
+  console.log('socketMap size : ' + socketMap.size);
+  console.log('idMap size : ' + idMap.size);
+  console.log('----------------------------');
+};
+/////////////^^^^^^^^^^^^^^^^^^^^^^^//////////////
+
+server.listen(9000);
+andServer.listen(9001);
 
 io.on('connection', (socket) => {
-  socket.emit(KEY.CONNECT, 'SUCCESS'); // 1010 연결 성공 응답
-  console.log(socket.id);
+  registSocket(socket);
+  socket.emit(
+    KEY.CONNECT,
+    JSON.stringify({
+      id: KEY.CONNECT,
+      status: 200,
+      message4: 'CONNECT SUCCESS',
+    })
+  ); // 1010 연결 성공 응답
+  printSocket();
 
-  socket.on('disconnect', () => {});
+  socket.on('disconnect', () => {
+    deleteSocket(socket);
+    printSocket();
+  });
   socket.on(KEY.SET_SHARE_ID, () => {
     // 1020 공유 디바이스의 아이디를 설정한다
     shareDevice = socket.id;
@@ -44,12 +194,22 @@ io.on('connection', (socket) => {
   });
   socket.on(KEY.GET_TREE_OF_FOLDERS, () => {
     // 2000 - 폴더 구조를 출력 요청
-    io.to(shareDevice).emit(
-      KEY.GET_TREE_OF_FOLDERS,
+    if (!checkSocket(shareDevice)) {
+      // 4000 - 공유 디바이스 연결이 되어있지 않음.
+      io.to(socket.id).emit(
+        KEY.NOT_SHARE_DEVICE,
+        JSON.stringify({
+          status: 204,
+          message: 'NOT SHARE DEVICE',
+        })
+      );
+      return;
+    }
+    idMap.get(shareDevice).write(
       JSON.stringify({
-        // 2000 - 폴더 구조를 출력 요청 전달
-        targetId: socket.id,
-      })
+        id: KEY.GET_TREE_OF_FOLDERS,
+        targetId: socketMap.get(socket),
+      }) + '\n'
     );
   });
   socket.on(KEY.RES_GET_TREE_OF_FOLDERS, (data) => {
@@ -61,21 +221,76 @@ io.on('connection', (socket) => {
   });
   socket.on(KEY.UPDATE_NAME_OF_FOLDER, () => {
     // 2001 - 폴더 이름 변경
-    io.to(shareDevice).emit({
-      targetId: socket.id,
-    });
+    if (!checkSocket(shareDevice)) {
+      // 4000 - 공유 디바이스 연결이 되어있지 않음.
+      io.to(socket.id).emit(
+        KEY.NOT_SHARE_DEVICE,
+        JSON.stringify({
+          status: 204,
+          message: 'NOT SHARE DEVICE',
+        })
+      );
+      return;
+    }
+    idMap.get(shareDevice).write(
+      JSON.stringify({
+        id: KEY.UPDATE_NAME_OF_FOLDER,
+        targetId: socketMap.get(socket),
+        path: './example/folder',
+        name: 'hello.txt',
+        newName: 'hello2.txt',
+      }) + '\n'
+    );
   });
   socket.on(KEY.RES_UPDATE_NAME_OF_FOLDER, (data) => {
     // 2001 - 폴더 이름 변경에 대한 응답(ex 폴더 트리 다시 제공)
   });
   socket.on(KEY.DELETE_FOLDERS, (data) => {
     // 2002 - 폴더 삭제
+    if (!checkSocket(shareDevice)) {
+      // 4000 - 공유 디바이스 연결이 되어있지 않음.
+      io.to(socket.id).emit(
+        KEY.NOT_SHARE_DEVICE,
+        JSON.stringify({
+          status: 204,
+          message: 'NOT SHARE DEVICE',
+        })
+      );
+      return;
+    }
+    idMap.get(shareDevice).write(
+      JSON.stringify({
+        id: KEY.DELETE_FOLDERS,
+        targetId: socketMap.get(socket),
+        path: './example/folder',
+        name: 'hello.txt',
+      }) + '\n'
+    );
   });
   socket.on(KEY.RES_DELETE_FOLDERS, (data) => {
     // 2002 - 폴더 삭제에 대한 응답(ex 폴더 트리 다시 제공)
   });
   socket.on(KEY.ADD_FOLDERS, (data) => {
     // 2003 - 폴더 추가
+    if (!checkSocket(shareDevice)) {
+      // 4000 - 공유 디바이스 연결이 되어있지 않음.
+      io.to(socket.id).emit(
+        KEY.NOT_SHARE_DEVICE,
+        JSON.stringify({
+          status: 204,
+          message: 'NOT SHARE DEVICE',
+        })
+      );
+      return;
+    }
+    idMap.get(shareDevice).write(
+      JSON.stringify({
+        id: KEY.ADD_FOLDERS,
+        targetId: socketMap.get(socket),
+        path: './example/folder',
+        name: 'hello.txt',
+      }) + '\n'
+    );
   });
   socket.on(KEY.RES_ADD_FOLDERS, (data) => {
     // 2003 - 폴더 추가에 대한 응답(ex 폴더 트리 다시 제공)
