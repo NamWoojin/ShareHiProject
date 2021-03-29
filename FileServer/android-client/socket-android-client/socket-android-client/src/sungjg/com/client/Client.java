3a1c6872d5bbec4a4ccfb9a598747dcba55abdb9
jaehyukshim <jaehyukawot@gmail.com>
2021-03-29 20:02:30 +0900

    Fix FileServer
    Former-commit-id: 21e95fb873f2c3ac4823a46cc4c3834f3e97e600

--- FileServer/android-client/socket-android-client/socket-android-client/src/sungjg/com/client/Client.java
+++ FileServer/android-client/socket-android-client/socket-android-client/src/sungjg/com/client/Client.java
@@ -3,8 +3,14 @@ package sungjg.com.client;
 import sugjg.com.dto.FileStat;
 
 public class Client {
-	public static final String IP = "j4f001.p.ssafy.io";
+	/*
+	 * public static final String IP = "j4f001.p.ssafy.io"; public static final int
+	 * PORT = 9003;
+	 */
+
+	public static final String IP = "localhost";
 	public static final int PORT = 9003;
+
 	public static void main(String[] args) {
 		FileStat fs = new FileStat();
 		SocketInfo s = new SocketInfo(fs);
