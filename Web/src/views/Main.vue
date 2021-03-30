8fa99c44f25cf1d15ba616e5f6361b754722e6cc
kwonsejin <kwon_sejin@naver.com>
2021-03-30 13:26:53 +0900

    Fix S04FIELD01-127 : Footer text 수정
    Former-commit-id: eb9dfe4b3fcd99595e3fcdd899885ef464348507

--- Web/src/views/Main.vue
+++ Web/src/views/Main.vue
@@ -1,13 +1,5 @@
 <template>
   <div style="width: 100%;">
-    <router-link :to="{ name: 'Storage'}" style="text-decoration: none; color: black;">Storage</router-link>
-    |
-    <router-link :to="{ name: 'Profile'}" style="text-decoration: none; color: black;">Profile</router-link>
-    |
-    <router-link :to="{ name: 'Signup'}" style="text-decoration: none; color: black;">Signup</router-link>
-    |
-    <router-link :to="{ name: 'Login'}" style="text-decoration: none; color: black;">Login</router-link>
-    <h2>Main (Advertisement)</h2>
     <Advertisement />
     <Footer />
   </div>
