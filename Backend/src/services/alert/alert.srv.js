28a14bc3c05cc4b43f8ca0a1b12555c6a53fb2d0
구민진 <whxorb44@gmail.com>
2021-04-01 14:51:20 +0900

    Update S04FIELD01-233 : 알림서비스
    Former-commit-id: 197dfa06a3403e1422a27c8ec849f6f85dcf4e38

--- Backend/src/services/alert/alert.srv.js
+++ Backend/src/services/alert/alert.srv.js
@@ -2,25 +2,44 @@
 
 const RabbitmqWrapper = require('../../modules/rabbitmq')
 const amqp = require('amqplib/callback_api')
+const { connection_id } = require('../../config/redis/redis.emailAuth')
 // const async = require('async');
 
 const sendMSG = async(req, res) => {
-    amqp.connect('amqp://j4f001.p.ssafy.io:5672', (err, connetion) => {
-        
+    amqp.connect('amqp://j4f001.p.ssafy.io:5672', (err1, connetion) => {
+        if(err1) {
+            console.log(err);
+            throw err;
+        }
+        connetion.createChannel((err2, channel) => {
+            if(err2) {
+                console.log(err2);
+                throw err;
+            }
+
+            let queue = 'hello';
+            let msg = 'hello world';
+            channel.assertQueue(queue, {
+                durable: false;
+            });
+            channel.sendToQueue(queue, Buffer.from(msg));
+            console.log('send %s', msg);
+        })
     })
-    const mem_id = req.query.mem_id;
-    try {
-        const url = 'amqp://j4f001.p.ssafy.io:5672';
-        const queueName = 'MQ_test' + mem_id;
-        const rq = new RabbitmqWrapper(url, queueName);
+    // serTimeout
+    // const mem_id = req.query.mem_id;
+    // try {
+    //     const url = 'amqp://j4f001.p.ssafy.io:5672';
+    //     const queueName = 'MQ_test' + mem_id;
+    //     const rq = new RabbitmqWrapper(url, queueName);
 
-        await rq.send_helloWorld();
+    //     await rq.send_helloWorld();
 
-        res.sendStatus(204);
-    } catch (e) {
-        console.error(e);
-        next(e);
-    }
+    //     res.sendStatus(204);
+    // } catch (e) {
+    //     console.error(e);
+    //     next(e);
+    // }
 }
 
 module.exports = {
