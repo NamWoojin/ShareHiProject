6b39cb19ea84e4244d0e3cb6308d8b60191ccf72
구민진 <whxorb44@gmail.com>
2021-04-01 14:28:40 +0900

    Update S04FIELD01-233 : rabbitmq수정
    Former-commit-id: d5075d460b562838ed4c349adc48f99e203a0e71

--- Backend/src/services/alert/alert.srv.js
+++ Backend/src/services/alert/alert.srv.js
@@ -1,10 +1,26 @@
 'use strict'
 
 const RabbitmqWrapper = require('../../modules/rabbitmq')
-const async = require('async');
+const amqp = require('amqplib/callback_api')
+// const async = require('async');
 
 const sendMSG = async(req, res) => {
+    amqp.connect('amqp://j4f001.p.ssafy.io:5672', (err, connetion) => {
+        
+    })
+    const mem_id = req.query.mem_id;
+    try {
+        const url = 'amqp://j4f001.p.ssafy.io:5672';
+        const queueName = 'MQ_test' + mem_id;
+        const rq = new RabbitmqWrapper(url, queueName);
 
+        await rq.send_helloWorld();
+
+        res.sendStatus(204);
+    } catch (e) {
+        console.error(e);
+        next(e);
+    }
 }
 
 module.exports = {
