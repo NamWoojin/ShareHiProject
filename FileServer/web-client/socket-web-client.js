61bb0e373edf8d799ed1ece6287d6be665bb2a07
jaehyukshim <jaehyukawot@gmail.com>
2021-04-03 00:20:18 +0900

    Add file server for download
    Former-commit-id: cac4b15b59f1fb578b8f037dd2d7719f9a393303

--- FileServer/web-client/socket-web-client.js
+++ FileServer/web-client/socket-web-client.js
@@ -190,110 +190,12 @@ socket.on(8000, (data) => {
 //   })
 // );
 
-/*
-let getConnection = function () {
-  console.log('connecting...');
-  socket = io.connect(config.host, {});
-  socket.on(2003, (data) => {
-    console.log('2003');
-    console.log(data);
-  });
-  socket.on(2002, (data) => {
-    console.log('2002');
-    console.log(data);
-  });
-  socket.on(2001, (data) => {
-    console.log('2001');
-    console.log(data);
-  });
-  socket.on(1010, (data) => {
-    console.log('1010');
-*/
-/**
- * 파일 전송 1단계. 정보 제공
- */
-/*
-     file.on('data', (data) => {
-       config.uploadedSize += data.length;
-       */
-/*
-    let stats = fs.statSync(config.filename);
-
-    let fileStat = {
-      path: './',
-      name: 'sample',
-      ext: '.mp4',
-      size: stats.size,
-    };
-    console.log(fileStat);
-*/
-/**
- * 먼저 파일 정보를 보낸다 - 7000
- */
-/*
-    socket.emit(7000, JSON.stringify(fileStat));
-  });
-  socket.on(4000, (data) => {
-    console.log('4000');
-    console.log(data);
-  });
-  socket.on(2000, (data) => {
-    console.log('2000');
-    console.log(data);
-  });
-  socket.on(4001, (data) => {
-    console.log('4001');
-    console.log(data);
-  });
-  socket.on(7000, (data) => {
-    console.log('7000');
-    //데이터를 보내야 한다
-    data = JSON.parse(data);
-    let file = fs.createReadStream(config.filename, { flags: 'r', start: data.tmpfileSize }); // default : 64 * 1024
-    file.on('data', (data) => {
-      socket.emit(7001, data);
-    });
-  });
-  socket.on(7001, (data) => {
-    console.log('7001');
-    data = JSON.parse(data);
-    console.log('progress bar : ' + data.percent);
-  });
-
-  socket.on('connect', () => {
-    if (socket.connected) console.log('서버로 성공적으로 연결되었습니다 : ' + config.host);
-    else console.log('서버의 연결이 끊겼습니다 : ' + config.host);
-  });
+const openFile = async () => {
+  try {
+    // Always returns an array.
+    const [handle] = await window.showOpenFilePicker();
+    return handle.getFile();
+  } catch (err) {
+    console.error(err.name, err.message);
+  }
 };
-*/
-// socket.emit(
-//   2000,
-//   JSON.stringify({
-//     path: 'root 또는 부모의 경로',
-//     name: '폴더의 이름',
-//   })
-// );
-// socket.emit(
-//   2001,
-//   JSON.stringify({
-//     path: 'root 또는 부모의 경로',
-//     name: '폴더의 이름',
-//     newName: '변경될 폴더의 이름',
-//   })
-// );
-// socket.emit(
-//   2002,
-//   JSON.stringify({
-//     path: 'root 또는 부모의 경로',
-//     name: '폴더의 이름',
-//   })
-// );
-// socket.emit(
-//   2003,
-//   JSON.stringify({
-//     path: 'root 또는 부모의 경로',
-//     name: '폴더의 이름',
-//   })
-// );
-
-// getConnection();
